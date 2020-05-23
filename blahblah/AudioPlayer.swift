6dc991c4e1889001ca84994d89fe9d84b9b0f84a
陈敏华 <chenminhua@cn-yutdu-0702.fareast.corp.microsoft.com>
2020-05-23 11:07:44 +0800

    blahblah

---
+++ blahblah/AudioPlayer.swift
@@ -0,0 +1,21 @@
+//
+//  AudioPlayer.swift
+//  blahblah
+//
+//  Created by 陈敏华 on 2020/5/23.
+//  Copyright © 2020 陈敏华. All rights reserved.
+//
+
+import SwiftUI
+
+struct AudioPlayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AudioPlayer_Previews: PreviewProvider {
+    static var previews: some View {
+        AudioPlayer()
+    }
+}
